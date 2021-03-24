5db1e88dcd09e0b234e279971993e455b3b6688f
Trevin Jared <trevin.jared@gmail.com>
2021-03-24 09:11:03 -0700

    save

---
+++ src/dux/store.js
@@ -0,0 +1,7 @@
+import {createStore} from 'redux';
+import movieReducer from './movieReducer';
+import {devToolsEnhancer} from 'redux-devtools-extension';
+
+let store = createStore(movieReducer, devToolsEnhancer());
+
+export default store
